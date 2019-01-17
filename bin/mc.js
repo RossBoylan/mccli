5e5f59f02da66703d3ae2d5b2afe6561322e5491
Ross Boylan <ross.boylan@ucsf.edu>
2019-01-17 13:57:16 -0800

    Start work on getting repeatable random numbers. This adds command line options to mc run to support it.  They don't do anything yet.

--- bin/mc.js
+++ bin/mc.js
@@ -9,10 +9,29 @@ let argv = yargs
     command: 'init',
     desc: 'initialize MC system in this directory',
     handler: init
-  })
+    })
 .command({
-    command: 'run-sims [iterations]',
+    command: 'run-sims [iterations] [start] [seed]',
     aliases: ['run', 'r'],
     desc: 'run MC simulations',
-    handler: runSims
-	}).argv
+    builder: (yargs) => yargs.positional('iterations', {
+        describe: 'Number of simulations to perform',
+        type: 'number',
+        default: 1000
+    })
+        .positional('start', {
+            describe: "Number of first simulation.  Simulations will range from start to start+iterations-1",
+            type: 'number',
+            default: 0
+        })
+        .positional('seed', {
+            describe: 'seed for random number generator.  Will be combined with simulation number.  ' +
+                'If omitted random numbers will not be reproducible, and may not be sufficiently independent' +
+                ' across simulations.',
+            type: 'number'
+        })
+        .epilog("All numbers should be unsigned integers."),
+    handler: runSims,
+})
+.help()
+.argv

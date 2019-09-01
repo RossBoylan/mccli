ae60d6e95446c67da8a415f2f03e7c412fccbaa4
Ross Boylan <ross.boylan@ucsf.edu>
2019-09-01 10:37:43 -0700

    testqt get desired custom output eliminates most options retains a lot of code I'm not using. This code only does first subgroup and does not display or write the results, except via print.

--- bin/python/testqt.py
+++ bin/python/testqt.py
@@ -22,6 +22,9 @@ import pdb
 import sys
 import random
 from PySide2 import QtCore, QtWidgets, QtGui, QtSql
+from PySide2.QtSql import QSqlQuery
+import pandas as pd
+import numpy as np
 
 DATABASE=r"C:\Users\rdboylan\Documents\KBD\bin\python\alldata.db"
 
@@ -42,9 +45,9 @@ class MyWidget(QtWidgets.QWidget):
         self.layout.addWidget(self.text)
         
         self._initVariables()
-        self._initOptions()
-        self._initStats()
-        self._initResults()
+        #self._initOptions()
+        #self._initStats()
+        #self._initResults()
         self.vVariables.selectionModel().selectionChanged.connect(self.variableSelectionChanged)
         self.setLayout(self.layout)
 
@@ -90,7 +93,7 @@ class MyWidget(QtWidgets.QWidget):
         "setup GUI and data for statistics to collect"
         self.vStatsGroup = QtWidgets.QGroupBox("Values to Report")
         innerLayout = QtWidgets.QHBoxLayout()
-        opts = [("total", True), ("avg", True), ("sd", False), ("min", True), ("max", True) ]
+        opts = [("", True), ("avg", True), ("sd", False), ("min", True), ("max", True) ]
         self._stats = {}
         for fld, default in opts:
             cb = QtWidgets.QCheckBox(fld)
@@ -113,7 +116,107 @@ class MyWidget(QtWidgets.QWidget):
         The arguments are <QItemSelection>s"""
         # selected.indexes() returns QModelIndexList, which is iterable yielding QModelIndex
         vars = [idx.data() for idx in selected.indexes()]
-        self._buildResults(vars)
+        self._buildResults2(vars)
+
+    def _buildResults2(self, vars):
+        """Build custom report in expected format and display it.
+        Within simulation, sum over all years.
+        Then rotate results so sex/age are columns.
+        Compute subtotals over sex and then overall.
+        Compute statistics by column.
+        Finally, append per simulation values.
+        vars is a list of strings, names of top level variables"""
+        vars = "('" +  "', '".join(vars) + "')"
+        groupVars = ["name", "subCat", "scenario", "iSim", "label"]
+        sqlGroups = ", ".join(groupVars)
+        strSQL = "SELECT "+sqlGroups+", TOTAL(value) as v" +\
+         " FROM data LEFT OUTER JOIN fullvar USING (fullvarid) LEFT OUTER JOIN variable " + \
+            "USING (varid) LEFT OUTER JOIN demo USING (demoid) " +\
+            " WHERE name IN " + vars + " GROUP BY "+sqlGroups +" ORDER BY "+ sqlGroups + ";"
+        print(strSQL)
+        q = QSqlQuery()
+        if not q.exec_("CREATE TEMP VIEW allyrs AS " + strSQL):
+            raise MyErr("Unable to create temp allyrs: "+q.lastError().text())
+        q2 = QSqlQuery()
+        if not q2.exec_("SELECT DISTINCT label FROM allyrs ORDER BY label;"):
+            raise MyErr("Unable to count demographics from temp allyrs: "+q2.lastError().text())
+        # q.size seems to always be -1
+        labels = []
+        while q2.next():
+            labels.append(q2.value(0))
+        nDemo = len(labels)
+
+        q = QSqlQuery()
+        q.setForwardOnly(True)
+        if not q.exec_("SELECT * FROM allyrs;"):
+            print(q.lastError())
+            return
+        # at this point we have v with totals across years
+        colNames = labels
+        self.df = pd.DataFrame(self._chunk(q, labels), columns=colNames)
+        self._addTotals()
+        self._addStats()
+        #self.df.to_string(float_format=(lambda a : np.format_float_positional(a, precision=2)))
+        print(self.df)
+        return self.df
+
+    def _chunk(self, q, labels):
+        """Rotate entries for one chunk and return a DataFrame.
+        q is a query result with the necessary data
+        labels are the column labels
+        a chunk is one particular variable, subCat, and scenario"""
+        chunkValues = None
+        t_row = [ ]  # transposed row
+        iSim = -1
+        last_label = labels[len(labels)-1]
+        while q.next():
+            thisKeys = [q.value(i) for i in range(3)]
+            if chunkValues:
+                if not chunkValues == thisKeys:
+                    # new chunk.  For now we just bail
+                    return
+            else:
+                chunkValues = thisKeys
+            if iSim < 0:
+                # start of this row
+                iSim = q.value(3)
+                if iSim>5:
+                    # shortcut for development
+                    return
+                t_row = [ ]
+            t_row.append(q.value(5))
+            if q.value(4) == last_label:
+                # one transposed row is ready
+                yield t_row
+                iSim = -1
+
+    def _addTotals(self) :
+          """add columns with totals for both sexes and overall.
+          The self.df is modified in place """
+          self._addMF('F')
+          self._addMF('M')
+          self._addAll()
+
+    def _addMF(self, sex):
+        cnms = self.df.columns
+        idx = [i for i in range(len(cnms)) if (cnms[i].startswith(sex) and cnms[i][1].isdigit())]
+        self.df.insert(self.df.shape[1], sex+" (all)", self.df.iloc[:, idx].sum(axis=1))
+
+    def _addAll(self):
+        ncol = self.df.shape[1]
+        # iloc arguments must be [] not ()
+        self.df.insert(ncol, "Everyone", self.df.iloc[:, [ncol-2, ncol-1]].sum(axis=1))
+
+
+    def _addStats(self):
+        """Compute requested statistics for self.df
+        and insert them at the top of same"""
+        # for now ignore requested stats and just give all
+        aSumDF = self.df.describe(percentiles=(0.05, .10, .25, .5, .75, .9, .95), include=[np.number])
+        self.df = pd.concat([aSumDF, self.df])
+
+
+
 
     def _buildResults(self, vars):
         """Build a results query and show it in vResults

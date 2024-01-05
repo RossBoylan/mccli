8dbee3b39ace660b367086b83eeac515ece7dfa6
Ross Boylan on EB-WS-12 <ross.boylan@ucsf.edu>
2024-01-05 12:20:16 -0800

    Add $ after filename to track new requirements of literate extension. The change was motivated by my problem report. This causes the generated v4-VaraibleSelector.js to catch up with the code changes already in .literate.

--- bin/js/v4-VariableSelector.js
+++ bin/js/v4-VariableSelector.js
@@ -1,8 +1,9 @@
-const sectionRE = /\[\s*(\S.*)\s*\]/i;
+const sectionRE = /\[\s*((?<full>(?<base>\S.*)(?<ext>\.csv))|(?<simple>\S.*)(?<=\S))\s*\]/i;
+const sectionNames = Set(["targets_output", "totresults", "calib"])
 const sepRE = /(\s*,\s*)|(\s+)/;
 module.exports = class VariableSelector {
    constructor(master, configfile) {
-      let i, line, match, csvname, current, v, rawv;
+      let i, line, match, csvname, base, ext, current, v, rawv;
       const reader = new nReadLines(configfile);
       while (line=reader.next()) {
          line = line.toString('ascii');
@@ -16,9 +17,22 @@ module.exports = class VariableSelector {
    
          if (match = line.match(sectionRE)){
             // process section heading
-            csvname = match[1];
-            if (csvname.slice(-4).toLowerCase() != ".csv")
-                  csvname += ".csv"
+            // ((?<full>(?<base>\S.*)(?<ext>\.csv))|(?<simple>\S.*)
+            csvname = match.groups.full;
+            if (csvname) {
+               base = match.group.base;
+               ext = match.group.ext;
+            } else {
+               base = match.group.simple;
+               if (! base)
+                  // Trying to use error() defined in runSims.js
+                  error("monte.conf has an empty section heading.");
+               ext = ".csv";
+               csvname = base + ext;
+            }
+            // at this poin base, ext and csvname are all set up.
+            if (!sectionNames.has(base.toLowerCase()))
+               error("monte.conf has section for ${base}.  Unknown; expected one of ${sectionNames}.");
             current = {
                   /* varPatterns has keys that are the raw text entered
                   and values that are regular expressions.

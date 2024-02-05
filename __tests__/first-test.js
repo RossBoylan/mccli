b3466a86a190a0276a46e2879bc9deb2c3a65e1f
Ross Boylan on EB-WS-12 <ross.boylan@ucsf.edu>
2024-02-05 14:33:50 -0800

    monte2.conf is valid; add more tests.

--- __tests__/first-test.js
+++ __tests__/first-test.js
@@ -13,14 +13,30 @@ while the reference to monte.conf below seems to have the project
 working directory. Maybe test() sets the context?
 */
 
-function newVS() {
-    let master = {};
-    return new VariableSelector(master, './__tests__/monte.conf') ;
+function newVS(master, fname) {
+    return new VariableSelector(master, fname) ;
 }
 
 test("check basic run with paths", () => {
 
    let errRE = /monte.conf has section for \w+\.  Unknown; expected one/
    // must wrap error in additional function call
-   expect(() => newVS()).toThrow(errRE);
+   expect(() => newVS({}, './__tests__/monte.conf')).toThrow(errRE);
+})
+
+test("basic scan of monte.conf", ()=>{
+    let master = {};
+    let vs = newVS(master, './__tests__/monte2.conf');
+    //toHaveLength doesn't work on Maps
+    expect(master.sources.size).toBe(2);
+    // check key converted to lower case
+    expect(master.sources.has("TARGETS_OUTPUT")).toBeFalsy();
+    expect(master.sources.has("targets_output")).toBeTruthy();
+    let sds = master.sources.get("targets_output");
+    //check case is preserved where appropriate
+    expect(sds.basename).toBe("targets_output");
+    expect(sds.ifname).toBe("TARGETS_OUTPUT.CSV");
+    /* currently no public interface to check for a match,
+    and the variable the patterns are held in is private.
+    */
 })

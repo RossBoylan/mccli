8039b92b9c2456db405eca397ae7621fdc81052b
Ross Boylan on EB-WS-12 <ross.boylan@ucsf.edu>
2024-02-05 19:42:06 -0800

    Add code to scan codebook.  Seems to be working in the debugger. Also added data with the codebook.

---
+++ bin/js/v4-Codebook.js
@@ -0,0 +1,38 @@
+'use strict';
+const nReadLines = require("n-readlines");
+//const error = require('./error');
+
+const lineRE = /^(\w+),(\w+\$?),\s*([^,]*)\s*,\s*$/;
+function realname(short){
+   if (short == "targets")
+      return "targets_output";
+   return short;
+}
+
+module.exports = class Codebook {
+   constructor(master, codebookPath){
+      const reader = new nReadLines(codebookPath);
+      let line = reader.next(); // skip first line with headers
+      let x, section, vname, des;  // will hold the match
+      while (line=reader.next()){
+         x = line.toString('ascii').match(lineRE);
+         if (!x){
+            line = line.toString('ascii');
+            continue;
+         }
+         [x, section, vname, des] = line.toString('ascii').match(lineRE);
+         if (section != this.section){
+            // New section in codebook
+            // Currently nothing to do to close previous section
+            // this.sds will be undefined if there is no entry, i.e., not of interest
+            this.section = section;
+            this.sds = master.sources.get(realname(section));
+         }
+         if (this.sds) {
+            if (! des)
+               des = vname;
+            this.sds.addVarDescription(vname, des);
+         }
+      }
+   }
+}
--- bin/js/v4.js
+++ bin/js/v4.js
@@ -2,10 +2,12 @@
 
 // class definitions
 const VariableSelector = require('./v4-VariableSelector');
+const Codebook = require('./v4-Codebook');
 
 module.exports = (yargs)=> {
    let master = {};
    let vs = new VariableSelector(master, './__tests__/monte2.conf');
+   let cb = new Codebook(master, './data/CVDPM_HF_Variable_List.csv');
    console.log(master.sources);
 
 }

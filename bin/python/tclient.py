4379a250fbc7020883dfb0ff57fcfbd0f50e2b33
Ross Boylan <rossboylan@stanfordalumni.org>
2025-06-12 17:12:41 -0700

    First attempt.  No output shown until everything finishes: /home/ross/UCSF/KBD/mccli/bin/python/tserver.py [This is  alice at iteration 0) This is  alice at iteration 1) This is  alice at iteration 2) ] alice returns 0 [This is  bob at iteration 0) This is  bob at iteration 1) This is  bob at iteration 2) ] bob returns 0

--- bin/python/tclient.py
+++ bin/python/tclient.py
@@ -1,4 +1,7 @@
 import sys
+from time import sleep
 
 who = sys.argv[1]
-print(who)
+for i in range(3):
+    sleep(3)
+    print(f'This is  {who} at iteration {i})')
--- bin/python/tserver.py
+++ bin/python/tserver.py
@@ -1,14 +1,20 @@
 import asyncio
 import sys
 
-async def main():
+async def do_one(name):
     p = await asyncio.create_subprocess_exec(
-        sys.executable, 'bin/python/tclient.py', "fred", "barney",
+        sys.executable, 'bin/python/tclient.py', name,
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE)
     stdout, stderr = await p.communicate()
     print(f'[{stdout.decode()}]')
     if stderr:
         print(f'[{stderr.decode()}]')
-    print(f'[{p.returncode}]')
+    print(f'{name} returns {p.returncode}')
+
+async def main():
+    await asyncio.gather(
+        do_one('alice'),
+        do_one('bob'),
+    )
 asyncio.run(main())

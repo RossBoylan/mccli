9a9f2aa218599b4f4549a7b5807821df0cd11692
Ross Boylan <rossboylan@stanfordalumni.org>
2025-06-13 06:32:35 -0700

    test real error output: passes
    This is bob at iteration 0
    This is alice at iteration 0
    ERR: bob dislikes iteration 1
    This is alice at iteration 1
    This is bob at iteration 2
    bob returns 0
    This is alice at iteration 2
    alice returns 0

--- bin/python/tclient.py
+++ bin/python/tclient.py
@@ -4,4 +4,7 @@ from time import sleep
 who = sys.argv[1]
 for i in range(3):
     sleep(3)
-    print(f'This is {who} at iteration {i}')
+    if i == 1 and who == 'bob':
+        print(f'{who} dislikes iteration {i}', file=sys.stderr)
+    else:
+        print(f'This is {who} at iteration {i}')

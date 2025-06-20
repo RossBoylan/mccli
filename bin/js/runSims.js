52853188a11f4df2b7495c0e8eb1e6a665e208cd
Ross Boylan on EB-WS-12 <ross.boylan@ucsf.edu>
2025-06-20 16:01:08 -0700

    Fix syntax errors.  Now runs pretty well, although still trouble with DETAIL. It keeps getting a type of stdout.  Maybe python can't parse it as JSON?
    {"type": "INFO", "text": "Maestro begins 2 parallel runs at 2025-06-20 15:50:12.334638\n"}
    {"type": "INFO", "text": "Debugger listening on ws://127.0.0.1:59845/472d31ea-edb2-4694-ab57-c3ecb9670f17", "runid": 1, "lasti": -1}
    {"type": "INFO", "text": "For help, see: https://nodejs.org/en/docs/inspector", "runid": 1, "lasti": -1}
    {"type": "INFO", "text": "Debugger listening on ws://127.0.0.1:59846/ce03b5bf-32d8-41ea-a884-e7d5762c2424", "runid": 2, "lasti": -1}
    {"type": "INFO", "text": "For help, see: https://nodejs.org/en/docs/inspector", "runid": 2, "lasti": -1}
    {"type": "INFO", "text": "Debugger attached.", "runid": 2, "lasti": -1}
    {"type": "INFO", "text": "Debugger attached.", "runid": 1, "lasti": -1}
    {"type": "stdout", "text": "{\"_\":[\"run\"],\"json\":true,\"overwrite\":true,\"python\":\"c:\\\\Users\\\\rdboylan\\\\Documents\\\\KBD\\\\mccli-repeatable\\\\pyenv\\\\Scripts\\\\python.exe\",\"py\":\"c:\\\\Users\\\\rdboylan\\\\Documents\\\\KBD\\\\mccli-repeatable\\\\pyenv\\\\Scripts\\\\python.exe\",\"iterations\":2,\"start\":0,\"$0\":\"C:\\\\Program Files\\\\nodejs\\\\node.EXE\",\"seed\":345,\"type\":\"ARGS\"}{\"type\":\"PROGRESS\",\"text\":\"simulations remaining: 1 eta:1.83m\",\"remaining\":1,\"iteration\":0,\"startTime\":1750459879400,\"endTime\":1750459989423,\"lastTime\":110023}", "runid": 2, "lasti": -1}
    {"type": "stdout", "text": "{\"_\":[\"run\"],\"json\":true,\"overwrite\":true,\"python\":\"c:\\\\Users\\\\rdboylan\\\\Documents\\\\KBD\\\\mccli-repeatable\\\\pyenv\\\\Scripts\\\\python.exe\",\"py\":\"c:\\\\Users\\\\rdboylan\\\\Documents\\\\KBD\\\\mccli-repeatable\\\\pyenv\\\\Scripts\\\\python.exe\",\"iterations\":2,\"start\":0,\"$0\":\"C:\\\\Program Files\\\\nodejs\\\\node.EXE\",\"seed\":345,\"type\":\"ARGS\"}{\"type\":\"PROGRESS\",\"text\":\"simulations remaining: 1 eta:1.86m\",\"remaining\":1,\"iteration\":0,\"startTime\":1750459883710,\"endTime\":1750459995123,\"lastTime\":111413}", "runid": 1, "lasti": -1}
    {"type": "SUMMARY", "text": "sum results"}
    {"type": "SUMMARY", "text": "done"}
    {"type": "SUMMARY", "text": "sum results"}
    {"type": "SUMMARY", "text": "done"}
    {"type": "stdout", "text": "{\"startTime\":\"6/20/2025, 3:51:19 PM\",\"endTime\":\"6/20/2025, 3:54:58 PM\",\"iterations\":2,\"model\":\"cvdmod92\",\"inp_files\":[\"Iq5u\"],\"dat_files\":[\"b\",\"bstk\",\"cst\",\"shrt\",\"qol\"],\"i0\":0,\"i1\":1,\"seed\":345,\"type\":\"DETAIL\"}{\"type\":\"DONE\",\"text\":\"  simulations completed in 3 minutes!\"}", "runid": 2, "lasti": -1}
    {"type": "INFO", "text": "Waiting for the debugger to disconnect...", "runid": 2, "lasti": -1}
    {"type": "stdout", "text": "{\"startTime\":\"6/20/2025, 3:51:23 PM\",\"endTime\":\"6/20/2025, 3:55:03 PM\",\"iterations\":2,\"model\":\"cvdmod92\",\"inp_files\":[\"Iq5d\"],\"dat_files\":[\"b\",\"bstk\",\"cst\",\"shrt\",\"qol\"],\"i0\":0,\"i1\":1,\"seed\":345,\"type\":\"DETAIL\"}{\"type\":\"DONE\",\"text\":\"  simulations completed in 3 minutes!\"}", "runid": 1, "lasti": -1}
    {"type": "INFO", "text": "Waiting for the debugger to disconnect...", "runid": 1, "lasti": -1}
    {"type": "INFO", "text": "Maestro finishes 2 parallel runs at 2025-06-20 15:55:51.190395\n"}

--- bin/js/runSims.js
+++ bin/js/runSims.js
@@ -39,13 +39,13 @@ let mylog = (msg, type="INFO") => {
 	})+'\n')
 	else
 		console.log(msg)
-}
+};
 
-let mylog_json (obj, type="INFO") => {
+let mylog_json = (obj, type="INFO") => {
 	/* obj is a message that is already in the form of
 	a JSON object */
 	obj.type = type
-	s = JSON.stringify(obj)
+	let s = JSON.stringify(obj)
 	if (json_logging)
 		process.stdout.write(s)
 	else

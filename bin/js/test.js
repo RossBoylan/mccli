eec4f2029a990872a535ce0912093a769fb11222
Ross Boylan on EB-WS-12 <ross.boylan@ucsf.edu>
2025-09-05 15:46:39 -0700

    Still closer to my plan for real code.  Shows problem clearly.
    (pyenv) PS C:\Users\rdboylan\Documents\KBD\mccli-parallel\bin\js> node test
    C:\Users\rdboylan\Documents\KBD\mccli-parallel\bin\js\test.js:28
                                    name: file_data.filename + ": DANGEROUS",
                                        ^
    
    SyntaxError: Unexpected token ':'
        at wrapSafe (node:internal/modules/cjs/loader:1662:18)
        at Module._compile (node:internal/modules/cjs/loader:1704:20)
        at Object..js (node:internal/modules/cjs/loader:1895:10)
        at Module.load (node:internal/modules/cjs/loader:1465:32)
        at Function._load (node:internal/modules/cjs/loader:1282:12)
        at TracingChannel.traceSync (node:diagnostics_channel:322:14)
        at wrapModuleLoad (node:internal/modules/cjs/loader:235:24)
        at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:171:5)
        at node:internal/main/run_main_module:36:49
    
    Node.js v22.17.1

--- bin/js/test.js
+++ bin/js/test.js
@@ -3,22 +3,25 @@ console.log("Hi\n");
 let file_data = {
     filename: "myfile"
 };
-let x = if (true) {
-    {
+let x = {
     value: file_data.filename,
     name: file_data.filename + ": DANGEROUS",
     description: "Changes to "+ file_data.filename + 
         " ordinarily require manual calibration.",
-    };
-};
-	
+    };	
 //var x = "myfile"; // even simpler test
+var all_dat_files = [
+    {filename: "myfile",
+        danger: true
+    },
+    {filename: "ordinary"
+    }
+]
 var whichDatFiles = {
 		type: 'checkbox',
 		message: 'select dat files to vary',
 		name: 'dat_files',
-		choices: [x, "ordinary"]
-        /*all_dat_files.map((file_data) => {
+		choices: all_dat_files.map((file_data) => {
 			if (file_data.danger){
 				{
 				value: file_data.filename,
@@ -29,7 +32,7 @@ var whichDatFiles = {
 			} else {
 				file_data.filename
 			}
-		}),*/
+		}),
 	};
 var questions = [ whichDatFiles ];
 inquirer.prompt(questions).then((answers) => {
